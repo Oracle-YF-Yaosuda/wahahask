77192320ca61bbd4e32c4ac589cf391f8ddf3254
sk19920518 <350458783@qq.com>
2015-12-04 16:09:40 +0800

    新建类

---
+++ Yaosuda/Yaosuda/ZizunViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZizunViewController.h
+//  Yaosuda
+//
+//  Created by oracle on 15/12/4.
+//  Copyright © 2015年 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZizunViewController : UIViewController
+
+@end
---
+++ Yaosuda/Yaosuda/ZizunViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZizunViewController.m
+//  Yaosuda
+//
+//  Created by oracle on 15/12/4.
+//  Copyright © 2015年 sk. All rights reserved.
+//
+
+#import "ZizunViewController.h"
+
+@interface ZizunViewController ()
+
+@end
+
+@implementation ZizunViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
