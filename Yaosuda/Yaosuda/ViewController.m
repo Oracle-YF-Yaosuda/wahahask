32da03dc76deb4de4e6ac586aea210a1d3c69760
xiaolang <langyaoyu@hotmail.com>
2016-01-08 14:24:28 +0800

    测试专用 记住账号及密码

--- Yaosuda/Yaosuda/ViewController.m
+++ Yaosuda/Yaosuda/ViewController.m
@@ -23,13 +23,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
+    
     self.user.delegate = self;
     self.pass.delegate = self;
     //设置圆角
     self.diview.layer.cornerRadius = 5.0;
     self.denglu.layer.cornerRadius = 5.0;
-    
+    /**
+     *  测试专用！！！！
+     */
     self.user.text = @"chengdu";
     self.pass.text = @"admin";
     
@@ -41,15 +43,13 @@
     else{
         NSString*user=[NSString stringWithFormat:@"%@",[[yonghuziliao getUserInfo] objectForKey:@"loginName"]];
         _user.text=user;
-//        NSString*pass=[NSString stringWithFormat:@"%@",[[yonghuziliao getUserInfo] objectForKey:@"password"]];
-//        _pass.text=pass;
-//        [self denglu:nil];
+        
     }
-
+    
     
     //键盘添加完成
     [KeyboardToolBar registerKeyboardToolBar:self.user];
-//    [KeyboardToolBar registerKeyboardToolBar:self.pass];
+    //    [KeyboardToolBar registerKeyboardToolBar:self.pass];
     
 }
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
@@ -60,13 +60,14 @@
     else
     {
         [self.view endEditing:YES];
+        [self denglu:nil];
     }
     return YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-   
+    
 }
 #pragma make - textfield Delegate
 -(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
@@ -134,69 +135,69 @@
     else if(![self PassWord:self.pass.text]){
         [WarningBox warningBoxModeText:@"请输入密码" andView:self.view ];
     }
-
+    
     else{
-    
-    
-    [WarningBox warningBoxModeIndeterminate:@"登录中..." andView:self.view];
-    
-    //获取设备唯一码
-    //    NSString *imei = [[UIDevice currentDevice].identifierForVendor UUIDString];
-    
-    //userID    暂时不用改
-    NSString * userID=@"0";
-    
-    //请求地址   地址不同 必须要改
-    NSString * url =@"/login";
-    
-    //时间戳
-    NSDate* dat = [NSDate dateWithTimeIntervalSinceNow:0];
-    NSTimeInterval a=[dat timeIntervalSince1970];
-    NSString *timeSp = [NSString stringWithFormat:@"%.0f",a];
-
-   
-    //将上传对象转换为json格式字符串
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/plain",@"text/html", nil];
-    SBJsonWriter *writer = [[SBJsonWriter alloc]init];
-    //出入参数：
-    NSDictionary*datadic=[NSDictionary dictionaryWithObjectsAndKeys:_user.text,@"loginName",_pass.text,@"password",@"867246020234069"/*imei*/,@"imei", nil];
-    
-    NSString*jsonstring=[writer stringWithObject:datadic];
-    
-    //获取签名
-    NSString*sign= [lianjie getSign:url :userID :jsonstring :timeSp ];
         
-    NSString *url1=[NSString stringWithFormat:@"%@%@%@%@",service_host,app_name,api_url,url];
-
-   // NSLog(@"%@",url1);
-    //电泳借口需要上传的数据
-    NSDictionary*dic=[NSDictionary dictionaryWithObjectsAndKeys:jsonstring,@"params",appkey, @"appkey",userID,@"userid",sign,@"sign",timeSp,@"timestamp", nil];
+        
+        [WarningBox warningBoxModeIndeterminate:@"登录中..." andView:self.view];
+        
+        //获取设备唯一码
+        //    NSString *imei = [[UIDevice currentDevice].identifierForVendor UUIDString];
+        
+        //userID    暂时不用改
+        NSString * userID=@"0";
+        
+        //请求地址   地址不同 必须要改
+        NSString * url =@"/login";
+        
+        //时间戳
+        NSDate* dat = [NSDate dateWithTimeIntervalSinceNow:0];
+        NSTimeInterval a=[dat timeIntervalSince1970];
+        NSString *timeSp = [NSString stringWithFormat:@"%.0f",a];
+        
+        
+        //将上传对象转换为json格式字符串
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/plain",@"text/html", nil];
+        SBJsonWriter *writer = [[SBJsonWriter alloc]init];
+        //出入参数：
+        NSDictionary*datadic=[NSDictionary dictionaryWithObjectsAndKeys:_user.text,@"loginName",_pass.text,@"password",@"867246020234069"/*imei*/,@"imei", nil];
+        
+        NSString*jsonstring=[writer stringWithObject:datadic];
+        
+        //获取签名
+        NSString*sign= [lianjie getSign:url :userID :jsonstring :timeSp ];
+        
+        NSString *url1=[NSString stringWithFormat:@"%@%@%@%@",service_host,app_name,api_url,url];
+        
+        // NSLog(@"%@",url1);
+        //电泳借口需要上传的数据
+        NSDictionary*dic=[NSDictionary dictionaryWithObjectsAndKeys:jsonstring,@"params",appkey, @"appkey",userID,@"userid",sign,@"sign",timeSp,@"timestamp", nil];
         //NSLog(@"ssssssssssssss%@",dic);
-    [manager GET:url1 parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        [WarningBox warningBoxHide:YES andView:self.view];
-        
-        [WarningBox warningBoxModeText:[NSString stringWithFormat:@"%@",[responseObject objectForKey:@"msg"]] andView:self.view];
-        NSLog(@"登录     /*/*/*/*/*/*/*/*/      %@",responseObject);
-        if ([[responseObject objectForKey:@"code"] intValue]==0000) {
-            NSString*path=[NSString stringWithFormat:@"%@/Documents/userInfo.plist",NSHomeDirectory()];
-            NSLog(@"%@",NSHomeDirectory());
+        [manager GET:url1 parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            [WarningBox warningBoxHide:YES andView:self.view];
             
-            NSDictionary*datadic=[responseObject valueForKey:@"data"];
-            [datadic writeToFile:path atomically:YES];
+            [WarningBox warningBoxModeText:[NSString stringWithFormat:@"%@",[responseObject objectForKey:@"msg"]] andView:self.view];
+            NSLog(@"登录     /*/*/*/*/*/*/*/*/      %@",responseObject);
+            if ([[responseObject objectForKey:@"code"] intValue]==0000) {
+                NSString*path=[NSString stringWithFormat:@"%@/Documents/userInfo.plist",NSHomeDirectory()];
+                NSLog(@"%@",NSHomeDirectory());
+                
+                NSDictionary*datadic=[responseObject valueForKey:@"data"];
+                [datadic writeToFile:path atomically:YES];
+                
+                navigation*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigation"];
+                [self presentViewController:chaxun animated:YES completion:^{
+                    [self setModalTransitionStyle: UIModalTransitionStyleCrossDissolve];
+                }];
+                
+            }
             
-            navigation*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigation"];
-            [self presentViewController:chaxun animated:YES completion:^{
-                [self setModalTransitionStyle: UIModalTransitionStyleCrossDissolve];
-            }];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            [WarningBox warningBoxHide:YES andView:self.view];
+            [WarningBox warningBoxModeText:@"网络连接失败！" andView:self.view];
             
-        }
-        
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        [WarningBox warningBoxHide:YES andView:self.view];
-        [WarningBox warningBoxModeText:@"网络连接失败！" andView:self.view];
-        
-    }];
+        }];
     }
 }
 
